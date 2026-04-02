e00ef0dd0af9ca4fdf48db46e077d4591d2e03c2
Amel <amelsekouane003@gmail.com>
2026-04-02 12:46:09 +0200

    Fonction array() + tests

--- ndarray/src/test/java/com/example/NDArrayTest.java
+++ ndarray/src/test/java/com/example/NDArrayTest.java
@@ -80,6 +80,7 @@ public class NDArrayTest {
         });
     }
 
+    //zeros()
     @Test
     void testZeros1D() {
         NDArray z = NDArray.zeros(4);
@@ -106,4 +107,52 @@ public class NDArrayTest {
     void testZerosInvalidShape() {
         assertThrows(IllegalArgumentException.class, () -> NDArray.zeros(2, -3));
     }
+
+    //array()
+    @Test
+    void testArray1D() {
+        float[] input = {1.0f, 2.0f, 3.0f};
+
+        NDArray array = NDArray.array(input);
+
+        assertEquals(1, array.getNdim());
+        assertArrayEquals(new int[]{3}, array.getShape());
+        assertEquals(3, array.getSize());
+
+        assertEquals(1.0f, array.get(0));
+        assertEquals(2.0f, array.get(1));
+        assertEquals(3.0f, array.get(2));
+    }
+
+    @Test
+    void testArray2D() {
+        float[][] input = {
+            {1.0f, 2.0f},
+            {3.0f, 4.0f}
+        };
+
+        NDArray array = NDArray.array(input);
+
+        assertEquals(2, array.getNdim());
+        assertArrayEquals(new int[]{2, 2}, array.getShape());
+        assertEquals(4, array.getSize());
+
+        assertEquals(1.0f, array.get(0, 0));
+        assertEquals(2.0f, array.get(0, 1));
+        assertEquals(3.0f, array.get(1, 0));
+        assertEquals(4.0f, array.get(1, 1));
+    }
+
+    @Test
+    void testArrayInvalidRows() {
+        float[][] input = {
+            {1.0f, 2.0f},
+            {3.0f}
+        };
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            NDArray.array(input);
+        });
+    }
+
 }
